585283c0d20c950dd1a60dfc2600c6fa19ffd958
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-06-13 23:46:12 -0400

    Create playRound function with game logic

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const CHOICES = ['ROCK', 'PAPER', 'SCISSORS']
 
+
+
+
 function getComputerChoice() {
     return CHOICES[Math.floor(Math.random()*CHOICES.length)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'It is a tie!';
+    } else if (playerSelection === 'ROCK'){
+        if (computerSelection === 'SCISSORS') {
+            return 'You win! Rock crushes scissors.';
+        } else {
+            return 'You lose! Paper covers rock.';
+        }
+    } else if (playerSelection === 'SCISSORS') {
+        if (computerSelection === 'ROCK') {
+            return 'You lose! Rock crushes scissors.';
+        } else {
+            return 'You win! Scissors cut paper.';
+        }
+    } else {
+        if (computerSelection === 'ROCK') {
+            return 'You win! Paper covers rock.';
+        } else {
+            return 'You lose! Scissors cut paper.';
+        }
+    }
 }
