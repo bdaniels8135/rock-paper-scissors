e46c470c9236accd9a357933118b2b1cde58cfcf
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-06-13 23:51:07 -0400

    Add console log of player and computer choices

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const CHOICES = ['ROCK', 'PAPER', 'SCISSORS']
+const CHOICES = [ 'ROCK', 'PAPER', 'SCISSORS']
 
 
 
@@ -8,25 +8,30 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    console.log(`You played ${playerSelection}. The computer played ${computerSelection}.`);
     if (playerSelection === computerSelection) {
         return 'It is a tie!';
-    } else if (playerSelection === 'ROCK'){
+    } else if (playerSelection ===  'ROCK'){
         if (computerSelection === 'SCISSORS') {
-            return 'You win! Rock crushes scissors.';
+            return 'You win! ROCK crushes SCISSORS.';
         } else {
-            return 'You lose! Paper covers rock.';
+            return 'You lose! PAPER covers ROCK.';
         }
     } else if (playerSelection === 'SCISSORS') {
-        if (computerSelection === 'ROCK') {
-            return 'You lose! Rock crushes scissors.';
+        if (computerSelection ===  'ROCK') {
+            return 'You lose! ROCK crushes SCISSORS.';
         } else {
-            return 'You win! Scissors cut paper.';
+            return 'You win! SCISSORS cut PAPER.';
         }
     } else {
-        if (computerSelection === 'ROCK') {
-            return 'You win! Paper covers rock.';
+        if (computerSelection ===  'ROCK') {
+            return 'You win! PAPER covers ROCK.';
         } else {
-            return 'You lose! Scissors cut paper.';
+            return 'You lose! SCISSORS cut PAPER.';
         }
     }
 }
+
+const playerSelection =  'ROCK';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
