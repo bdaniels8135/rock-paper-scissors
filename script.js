098d359047008f131195189421a302618e03ad7c
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-06-18 00:19:37 -0400

    Declare consts for ui elements

--- script.js
+++ script.js
@@ -17,49 +17,64 @@ const ROUND_OUTCOMES = Object.freeze({
 
 // UI Functions
 
-function printRoundMessage(playerMove, computerMove, roundOutcome, playerScore, computerScore, round) {
-    let outcomeMessage = 'This round is a tie!';
-    if (roundOutcome === ROUND_OUTCOMES.WIN) {
-        outcomeMessage = 'You win this round!';
-    }
-    if (roundOutcome === ROUND_OUTCOMES.LOSE) {
-        outcomeMessage = 'You lose this round!';
-    }
-    
-    roundMessage = 
-    `Round ${round}
-    You played ${MOVES[playerMove]} and the computer played ${MOVES[computerMove]}.
-    ${outcomeMessage}
-    The current score is Player ${playerScore} Computer ${computerScore}.`;
-    
-    console.log(roundMessage);
- }
+const rockButton = document.querySelector('#button-rock');
+const paperButton = document.querySelector('#button-paper');
+const scissorsButton = document.querySelector('#button-scissors');
+const playerMoveDisplay = document.querySelector('#player-move-display');
+const computerMoveDisplay = document.querySelector('#computer-move-display');
+const roundOutcomeDisplay = document.querySelector('#round-outcome-display');
+const roundNumber = document.querySelector('#round-number');
+const playerScore = document.querySelector('#player-score');
+const computerScore = document.querySelector('#computer-score');
+const gameOutcomeMessage = document.querySelector('#game-outcome-message');
+const gameResetButton = document.querySelector('#game-reset-button');
 
-function getPlayerMove() {
-    let playerInput;
-    while (true) {
-        playerInput = prompt('Type your selection: ROCK, PAPER, or SCISSORS.');
-        if (playerInput === null) {
-            console.log('Quitter!');
-            return;
-        }
-        if (!MOVES[playerInput.toUpperCase()]) {
-            alert('Please enter a valid choice to continue playing or click cancel to quit.');
-            continue;
-        }
-        return MOVES[playerInput.toUpperCase()];
-    }
-}
 
-function printFinalMessage(playerScore, computerScore) {
-    let finalMessage;
-    if (playerScore > computerScore) {
-        finalMessage = 'Congratulations! You beat the computer!';    
-    } else if (playerScore < computerScore) {
-        finalMessage = 'How disappointing! The computer beat you...';
-    }
-    console.log(finalMessage);
-}
+
+
+// function printRoundMessage(playerMove, computerMove, roundOutcome, playerScore, computerScore, round) {
+//     let outcomeMessage = 'This round is a tie!';
+//     if (roundOutcome === ROUND_OUTCOMES.WIN) {
+//         outcomeMessage = 'You win this round!';
+//     }
+//     if (roundOutcome === ROUND_OUTCOMES.LOSE) {
+//         outcomeMessage = 'You lose this round!';
+//     }
+    
+//     roundMessage = 
+//     `Round ${round}
+//     You played ${MOVES[playerMove]} and the computer played ${MOVES[computerMove]}.
+//     ${outcomeMessage}
+//     The current score is Player ${playerScore} Computer ${computerScore}.`;
+    
+//     console.log(roundMessage);
+// }
+
+// function getPlayerMove() {
+//     let playerInput;
+//     while (true) {
+//         playerInput = prompt('Type your selection: ROCK, PAPER, or SCISSORS.');
+//         if (playerInput === null) {
+//             console.log('Quitter!');
+//             return;
+//         }
+//         if (!MOVES[playerInput.toUpperCase()]) {
+//             alert('Please enter a valid choice to continue playing or click cancel to quit.');
+//             continue;
+//         }
+//         return MOVES[playerInput.toUpperCase()];
+//     }
+// }
+
+// function printFinalMessage(playerScore, computerScore) {
+//     let finalMessage;
+//     if (playerScore > computerScore) {
+//         finalMessage = 'Congratulations! You beat the computer!';    
+//     } else if (playerScore < computerScore) {
+//         finalMessage = 'How disappointing! The computer beat you...';
+//     }
+//     console.log(finalMessage);
+// }
 
 
 // Game Logic Functions
